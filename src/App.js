602d66c69933189e4caac100bd7f8286da5dc33c
Nishant Gupta <ngofficial99@gmail.com>
2024-09-05 17:09:43 +0530

    updated code added oriserve heading

--- src/App.js
+++ src/App.js
@@ -26,8 +26,10 @@ function App() {
 
   return (
     <div className="app-container">
+        <h1>Oriserve</h1>
       <h1>Todo App</h1>
       <h2>By Nishant Gupta</h2>
+    
       <div className="input-container">
         <input
           type="text"
